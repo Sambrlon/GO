466756cf360df74f6d9eea0559e13637523353a9
Evgeny Vlasov <33553003+vlasove@users.noreply.github.com>
2018-06-02 21:48:16 +0300

    Add files 13-16 of Chapter1

---
+++ goprogramming/constants/const.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const World = "Evgen"
+	fmt.Println("Hello, ", World)
+	fmt.Println("Happy ", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Yes, you can learn go! It's ", Truth)
+
+}
